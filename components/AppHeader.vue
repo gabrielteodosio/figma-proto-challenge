a0dd925e08f92f9471792a99a76d3e92cb882368
Gabriel T Marques <gabrieltmm@gmail.com>
2023-11-28 04:11:17 -0300

    feat : finished AppHeader component

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,25 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div>
-    <img :class="[$style]" src="~/assets/ClarifionLogoHeader.png" alt="Clarifion Logo" />
+  <div
+    class="w-full h-24 px-[127px] py-[30px] bg-white justify-between items-center inline-flex"
+  >
+    <img
+      class="w-48 h-9"
+      alt="Clarifion Logo"
+      src="~/assets/ClarifionLogoHeader.png"
+    />
+    <div class="justify-start items-start gap-8 flex">
+      <img
+        alt="McAfee Logo"
+        src="~/assets/McAfeeLogopngHeader.png"
+        class="w-[82px] h-8 relative"
+      />
+      <img
+        alt="Norton Logo"
+        src="~/assets/NortonLogoHeader.png"
+        class="w-[82px] h-8 relative"
+      />
+    </div>
   </div>
 </template>
 
@@ -13,5 +31,10 @@ export default {
 </script>
 
 <style module>
+.HeaderContainer {
+  background-color: red;
+}
 
+.HeaderLogo {
+}
 </style>
