4170ed1093cb1d51612e5323e9be02fda4ad7bd1
Gabriel T Marques <gabrieltmm@gmail.com>
2023-11-27 22:58:51 -0300

    chore : removed Logo, tutorial component added USPSHeader component, removed content folder and changed index page title

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
-  <Tutorial />
+  <USPSHeader />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import USPSHeader from '~/components/USPSHeader.vue'
 
 export default Vue.extend({
   name: 'IndexPage',
+  components: { USPSHeader },
+  head() {
+    return {
+      title: 'Challenge - Figma Prototype'
+    }
+  },
 })
 </script>
