dc60859a49b8c1c2c05a68169d69c39f8b7d6344
Gabriel T Marques <gabrieltmm@gmail.com>
2023-11-28 05:00:07 -0300

    feature : finished stepper component

---
+++ components/StepperComponent.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="w-full flex justify-center">
+    <div
+      class="w-5/6 h-10 mt-[3.75rem] justify-between items-center inline-flex"
+    >
+      <div class="justify-start items-center gap-5 flex">
+        <div
+          class="w-10 h-10 justify-center items-center flex bg-lime-400 rounded-full border-2 border-lime-400"
+        >
+          <Icon icon="charm:tick" class="w-7 h-7" color="white" />
+        </div>
+        <div
+          class="text-black text-xl font-normal font-['Manrope'] capitalize leading-snug"
+        >
+          Step 1 : Cart Review
+        </div>
+      </div>
+      <div class="justify-start items-center gap-5 flex">
+        <div
+          class="w-10 h-10 bg-lime-400 justify-center items-center flex rounded-full border-2 border-lime-400"
+        >
+          <Icon icon="charm:tick" class="w-7 h-7" color="white" />
+        </div>
+        <div
+          class="text-black text-xl font-normal font-['Manrope'] capitalize leading-snug"
+        >
+          Step 2 : Checkout
+        </div>
+      </div>
+      <div class="justify-start items-center gap-5 flex">
+        <div
+          class="w-10 h-10 bg-blue-500 flex items-center justify-center rounded-full border-2 border-blue-500"
+        >
+          <div
+            class="text-white text-xl font-normal font-['Manrope'] capitalize leading-snug"
+          >
+            3
+          </div>
+        </div>
+        <div
+          class="text-black text-xl font-bold font-['Manrope'] capitalize leading-snug"
+        >
+          Step 3 : Special Offer
+        </div>
+      </div>
+      <div class="justify-start items-center gap-5 flex">
+        <div
+          class="w-10 h-10 flex items-center justify-center rounded-full border-2 border-blue-500"
+        >
+          <span
+            class="text-blue-500 text-xl font-normal font-['Manrope'] capitalize leading-snug"
+          >
+            4
+          </span>
+        </div>
+        <div
+          class="text-black text-xl font-normal font-['Manrope'] capitalize leading-snug"
+        >
+          Step 4 : Confirmation
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Icon } from '@iconify/vue2'
+
+export default {
+  name: 'StepperComponent',
+  components: {
+    Icon,
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,25 +4,29 @@
     <AppHeader />
 
     <h1
-      class="text-black text-5xl font-normal font-['Manrope'] capitalize leading-[48px] text-center"
+      class="text-black text-5xl font-['Manrope'] capitalize leading-[48px] text-center"
     >
       Wait ! your order in progress.
     </h1>
     <h2
-      class="text-neutral-600 text-2xl font-normal font-['Manrope'] capitalize leading-normal text-center"
+      class="text-neutral-600 text-2xl font-['Manrope'] capitalize leading-normal text-center"
     >
       Lorem ipsum dolor sit amet, consectetur adipiscing
     </h2>
+
+    <StepperComponent />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import USPSHeader from '~/components/USPSHeader.vue'
+
 import AppHeader from '~/components/AppHeader.vue'
+import USPSHeader from '~/components/USPSHeader.vue'
+import StepperComponent from '~/components/StepperComponent.vue'
 
 export default Vue.extend({
   name: 'IndexPage',
-  components: { USPSHeader, AppHeader },
+  components: { USPSHeader, AppHeader, StepperComponent },
 })
 </script>
